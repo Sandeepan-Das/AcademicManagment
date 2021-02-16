4dd8e49b42b7a96c21b22f827c33ca672ccc19ce
Sandeepan-Das <sandeepandas2000@gmail.com>
2021-02-16 12:49:48 +0530

    Added login route

--- backEnd/External/pass.js
+++ backEnd/External/pass.js
@@ -1,6 +1,6 @@
 const bcryptjs = require('bcryptjs');
 const pass_hide = async (password)=>{
-    const hidden = await bcryptjs.hash("password",8)
+    const hidden = await bcryptjs.hash(password,8)
     
     return hidden;
 }
--- backEnd/Routes/user.js
+++ backEnd/Routes/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcryptjs = require("bcryptjs");
 
 const router = express.Router();
 
@@ -7,23 +8,58 @@ const verifyLogin = require("../middlewares/verify");
 
 const hid_pass = require("../External/pass");
 const fetch_token = require("../External/token");
+const { verify } = require("jsonwebtoken");
 
 router.post("/register", verifyLogin, async (req, res) => {
   const password = await hid_pass(req.body.password);
   const token = await fetch_token(req.isStudent, req.primaryKey);
   var sql = "INSERT INTO userdata (name,email,password,token) VALUES (?,?,?,?)";
-
+  var sql2 = `SELECT * FROM userdata WHERE email=?`;
   try {
     connection.query(
       sql,
       [req.body.name, req.body.email, password, token],
       (err, result) => {
         if (err) throw err;
-        else console.log("1 record inserted");
+        else {
+          connection.query(sql2, [req.body.email], async (err, result) => {
+            if (err) throw err;
+            else {
+              res.send({ token: result[0].token });
+            }
+          });
+        }
       }
     );
   } catch (error) {}
 });
+
+router.post("/login", verify, async (req, res) => {
+  var sql = `SELECT * FROM userdata WHERE email=?`;
+  try {
+    connection.query(sql, [req.body.email], async (err, result) => {
+      if (err) throw err;
+      else {
+        if (result.length == 0) res.sendStatus(404);
+        else {
+          const matchPass = await bcryptjs.compare(
+            req.body.password,
+            result[0].password
+          );
+
+          if (!matchPass) {
+            res.sendStatus(404);
+          } else {
+            res.send({ token: result[0].token });
+          }
+        }
+      }
+    });
+  } catch (err) {}
+});
+
+
+
 
 router.get("/display", async (req, res) => {
   var sql = "SELECT * FROM userdata";
