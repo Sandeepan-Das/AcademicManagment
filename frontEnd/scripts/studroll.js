d731f480069a95aa273c60bf09ff76e19c08a3da
Biswajit Rout <b119018@iiit-bh.ac.in>
2021-03-04 00:01:07 +0530

    adding marks page

---
+++ frontEnd/scripts/studroll.js
@@ -0,0 +1,15 @@
+function fetchStudDetails(roll) {
+    
+    const data = fetchURL();
+  
+    location.href = `/studMark?ID=${data.ID}&subj=${subj}&roll=${roll}`;
+  }
+  
+  function fetchURL() {
+    const params = new URLSearchParams(window.location.search);
+    
+    return {
+      ID: params.get("ID"),
+    };
+  }
+  
